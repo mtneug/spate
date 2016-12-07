b7bc68ae673c16f5b2d27f49279b51990d6d9ac7
Matthias Neugebauer <mtneug@mailbox.org>
2016-12-07 16:26:37 +0100

    Add package documentation

--- docker/docker.go
+++ docker/docker.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package docker contains code for integrating with Docker.
 package docker
 
 import (
